5e226a00f5ce652618fe08e74f7bfdeca2d1a8ab
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 22:25:15 -0700

    Add files via upload

---
+++ find_average_number.py
@@ -0,0 +1,8 @@
+# Write a program to find the average of numbers in a list in Python?
+list1 = [1, 2, 3, 4, 5, 6]  # the list it gave us
+i = 0
+for each in list1:
+    i += each
+
+print(i / len(list1))
+
