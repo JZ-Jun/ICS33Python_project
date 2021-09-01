c7325f0df28c7617509b3e463ff3e414c389c193
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:32:14 -0700

    Update Dynamic_Programming.py

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -1,7 +1,9 @@
-# Using Dynamic Programming:  Giving user input which is string s , return the longest palindromic substring in s.
-#Constraints:
-#1 <= s.length <= 1000
-#s consist of only digits and English letters.
+'''
+Using Dynamic Programming:  Giving user input which is string s , return the longest palindromic substring in s.
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+'''
 
 def ifpalstr(str1):
     x = len(str1)
