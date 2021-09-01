5c5aed0c259e25326e11059ddcc9e47abc1a70b7
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:01:44 -0700

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,45 @@
+# Jun Zhu
+def ifpalstr(str1):
+    x = len(str1)
+    if x % 2 == 0:
+        for i in range(x // 2):
+            if str1[i] != str1[-i - 1]:
+                return False
+    else:
+        for i in range(x // 2):
+            if str1[i] != str1[-i - 1]:
+                return False
+    return str1
+
+
+def longestpalstr(str1):
+    x = len(str1)
+    substr = ""
+    i = 1
+    if x == 1:
+        return str1
+    if ifpalstr(str1):
+        return ifpalstr(str1)
+    while i < x:
+        a = longestpalstr(str1[0:i])
+        b = longestpalstr(str1[i:])
+        if len(a) > len(substr):
+            substr = a
+        if len(b) > len(substr):
+            substr = b
+        i += 1
+    return substr
+
+
+x = input("s = ")
+i = 0
+if len(x) > 1000:
+    i = 1
+    print("wrong input")
+for each in x:
+    if each not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890":
+        print("wrong input")
+        i = 1
+        break
+if i == 0:
+    print(longestpalstr(x))
