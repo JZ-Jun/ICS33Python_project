41106c6f0f317652e4443c1ec0abaec63b647cd6
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:34:36 -0700

    Update count_palindrome_numbers.py

--- count_palindrome_numbers.py
+++ count_palindrome_numbers.py
@@ -1,7 +1,9 @@
-# A palindrome is a word, number, phrase, or other sequences of characters which reads the same backward as forward, such as racecar, mom, or 121.
-# Given a list, count and print all the palindrome numbers in it.
-# Example:
-# Input: 10 121 133 155 141 252 Output: 121 141 252 Total palindrome nos. are 3
+'''
+A palindrome is a word, number, phrase, or other sequences of characters which reads the same backward as forward, such as racecar, mom, or 121.
+Given a list, count and print all the palindrome numbers in it.
+Example:
+Input: 10 121 133 155 141 252 Output: 121 141 252 Total palindrome nos. are 3
+'''
 
 def countpalnum(list1):
     i = 0
