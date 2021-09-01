15e270c512a68937f80cf65de649dbea71c973be
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:08:19 -0700

    Update and rename program 5 - 2.py to recursion.py

--- recursion.py
+++ recursion.py
@@ -1,3 +1,11 @@
+#Given a value N, if we want to make change for N cents, and we have an infinite supply of each of S = { S1, S2, .. , Sm} valued coins
+#how many ways can we make the change? The order of coins doesn’t matter.
+#For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4. 
+#For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. so the output should be 5.
+#The idea is to use recursion to solve this problem. We recur to see if the total can be reached by choosing the coin or not for each coin of given denominations.
+#If choosing the current coin results in the solutions, update the total number of ways.
+
+
 def countways(n,s):
     
     list1=findways(n,s) # the ways of making the change (a list of list) 
