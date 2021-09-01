ad6f7cb7c9c52204c4d0fe49a53e5809b4c0a6ee
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:09:06 -0700

    Add files via upload

---
+++ program 6.py
@@ -0,0 +1,81 @@
+
+
+def lowcost(n,edges,src,dst,k):
+    dict1={} 
+
+    
+    for each in edges:
+        if each[0] not in dict1:
+            dict1[each[0]]=[each[1:]]    
+        else:
+            dict1[each[0]].append(each[1:])
+
+    # dict1 = {0: [[1, 100], [2, 500]], 1: [[2, 100]]}
+    
+    list1=[]
+
+    
+    for each in dict1[src]:
+        list1.append(each)
+
+     # list1 = [[1, 100], [2, 500]]
+
+
+
+    
+    prise=-1 # If there is no such route, output -1.
+    
+    for i in range(k+1): #  find the cheapest price from src to dst with up to k stops
+        
+        for each in range(len(list1)):
+            
+            if list1[0][0]==dst: # If list1[0][0] is the destination.
+                if prise==-1:
+                    prise=list1[0][1] # If no other price has been recorded before, record this latest
+                else:
+                    prise=min(prise,list1[0][1]) # Compare the previously recorded prices, and then record the cheaper one.
+
+            else: # If list1[0][0] is not the destination.
+                if list1[0][0] in dict1:
+                    
+                    for each1 in dict1[list1[0][0]]:
+                        
+                        if each1[1]+prise>=prise and prise!=-1: # If this price is more expensive than the previously recorded price, ignore it.
+                            
+
+                            continue #  rejects all the remaining statements in the current iteration of the loop and moves back to the top of the loop. 
+                            
+                        
+                        list1.append([each1[0],each1[1]+list1[0][1]]) 
+                        
+            list1.pop(0)
+
+    return prise
+
+
+
+# get input
+
+n=int(input("n=")) # how many cities
+edge=input("edges = ") # price between the 2 cities. for example:  [[0,1,100],[1,2,100],[0,2,500]]
+edge=edge[2:-2].split("],[")
+edges=[] 
+
+for each in edge: # Put the user input in the correct format
+    x=each.split(",")
+    y=[]
+    for each1 in x:
+        y.append(int(each1))
+    edges.append(y)
+    
+src=int(input("src = ")) # starting city
+dst=int(input("dst = ")) # destination
+k=int(input("k = ")) # up to k stops
+print(lowcost(n,edges,src,dst,k))
+
+## test cases:
+##
+##print(lowcost(3,[[0,1,100],[1,2,100],[0,2,500]],0,2,1))
+##print(lowcost(3,[[0,1,100],[1,2,100],[0,2,500]],0,2,0))
+##
+##
