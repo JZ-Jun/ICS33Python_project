0d8490b73ac453e83af4a4fd01ce69b036752d0a
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 23:36:03 -0700

    Update find_product_elements.py

--- find_product_elements.py
+++ find_product_elements.py
@@ -1,8 +1,10 @@
-#Get the product of all other elements
-#Given an array of integers, return a new array such that each element at index I of the new array is the product of all the numbers in the original array except the one at i.
-#For example:
-#if our input is [1,2,3,4,5], the expected output would be [120, 60,40,30,24]
-#if our input [3,2,1] the expected output be [2,3,6]
+'''
+Get the product of all other elements
+Given an array of integers, return a new array such that each element at index I of the new array is the product of all the numbers in the original array except the one at i.
+For example:
+if our input is [1,2,3,4,5], the expected output would be [120, 60,40,30,24]
+if our input [3,2,1] the expected output be [2,3,6]
+'''
 
 def turnli(x):
     outlist = []
