42e2240e824a11cac91841bbef64e500f99f6596
Jun Zhu <49256407+issac0723@users.noreply.github.com>
2021-08-31 22:30:42 -0700

    Update and rename program 2 tic tac toe.py to Program2_tic _tac_toe.py

--- Program2_tic _tac_toe.py
+++ Program2_tic _tac_toe.py
@@ -1,4 +1,20 @@
-# Team member: Tongze Mao, Jun Zhu, Zhuoran Liu
+#Tic tac toe is a very popular game.  Only two players can play at a time. 
+
+#Game Rules
+
+#Traditionally the first player plays with "X". So you can decide who wants to go with "X" and who wants to go with "O".
+#Only one player can play at a time.
+#If any of the players have filled a square then the other player and the same player cannot override that square.
+#There are only two conditions that may match will be a draw or may win.
+#The player that succeeds in placing three respective marks (X or O) in a horizontal, vertical, or diagonal row wins the game.
+ 
+#Winning condition
+#Whoever places three respective marks (X or O) horizontally, vertically, or diagonally will be the winner.
+#Submit your code and screenshots of your code in action. 
+#Hints :
+#Have a function that draws the board 
+#Have a function that checks position if empty or not
+#Have a function that checks player or won or not 
 
 import re  # With the re module you can use ideas  of match or search.
 
